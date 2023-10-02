98ed5c69bccda7f851ff4316b5be05fed6570301
HungryAmoeba <charles.xu@yale.edu>
2023-10-02 11:23:33 -0400

    completed scattering transform with saving

--- blis/models/scattering_transform.py
+++ blis/models/scattering_transform.py
@@ -16,48 +16,93 @@ def scattering_transform(x, scattering_type, wavelets, num_layers, highest_momen
     scattering_type - a string of either "blis" or "modulus"
     wavelets - a np array of wavelets (possibly containing the lowpass as a wavelet)
     num_layers - The number of wavelet matricies in each transform paths
-    x - a torch tensor of shape N x num_features
-    save_dir: a directory to the data. it should contain information about scattering type, and wavelets
+    x - a torch tensor of shape num_signals x N x num_features
+    save_dir: a directory to the data. it should contain information about scattering type, and wavelets (i.e. highest scale)
     '''
     if scattering_type not in ["blis", "modulus"]:
         raise ValueError("Invalid scattering type. Accepted values are 'blis' or 'modulus'.")
     
-    N ,num_features = x.shape 
+    num_signals, N ,num_features = x.shape 
     J = len(wavelets)
     
-    if scattering_type == 'blis':
-        combinations = list(product(range(J), [relu, reverse_relu], repeat = num_layers))
-    else:
-        combinations = list(product(range(J), [np.abs], repeat = num_layers)) 
+    # if scattering_type == 'blis':
+    #     combinations = list(product(range(J), [relu, reverse_relu], repeat = num_layers))
+    # else:
+    #     combinations = list(product(range(J), [np.abs], repeat = num_layers)) 
 
-    # store the outputs in a dictionary
-    coeff_dict = {}
-    for layer in range(num_layers):
-        for moment in range(1, highest_moment + 1):
-            coeff_dict[(layer,moment)] = list()
+    # # store the outputs in a dictionary
+    # coeff_dict = {}
+    # for layer in range(num_layers):
+    #     for moment in range(1, highest_moment + 1):
+    #         coeff_dict[(layer,moment)] = list()
+    
+    # num_layers is the LARGEST layer size
+    # layer_num is the largest layer size within the loop
+    # layer is the layer number looping up to layer_num 
+    for layer_num in range(1, num_layers+1):
+        # note that this code has redundant calculations for each layer!
+        if scattering_type == 'blis':
+            combinations = list(product(range(J), [relu, reverse_relu], repeat = layer_num))
+            num_activation = 2
+        else:
+            combinations = list(product(range(J), [np.abs], repeat = layer_num))
+            num_activation = 1
+
+        # store the output
+        coeffs = np.zeros((num_signals, (J*num_activation)**layer_num, num_features, highest_moment))
+
+        for ind, comb in enumerate(combinations):
+            layer_out = x 
+            for layer in range(layer_num):
+                wavelet_index = comb[layer * 2]
+                activation = comb[layer * 2 + 1]
+                wavelet = wavelets[wavelet_index]
+                wavelet_transform = np.einsum('ik, nkf->nif', wavelet, layer_out)
+                layer_out = activation(wavelet_transform)
+            
+            # the scattering transform along one path has now been calculated for all signals
+            # layer_out has shape [num_signals, num_vertices, num_features]
+            for moment in range(1, highest_moment + 1):
+                coeffs[:, ind, :, moment-1] = np.sum(np.power(layer_out, moment), axis = 1)
+        
+        # all of the coeffs have been calculated for a given layer and number of moments
+        # write them to memory
+
+        #create a directory for each layer 
+        layer_dir = os.path.join(save_dir, f'layer_{layer_num}')
+        if not os.path.exists(layer_dir):
+            os.makedirs(layer_dir)
+        for moment_ind in range(highest_moment):
+            full_path = os.path.join(layer_dir, f"moment_{moment_ind + 1}.npy")
+            np.save(full_path, coeffs[:,:,:, moment_ind])
+        
+        
+
+
             
 
-    # save each layers output and each statistical moment separately
-    for ind, comb in enumerate(combinations):
-        layer_out = x 
-        for layer in range(num_layers):
-            wavelet_index = comb[layer * 2]
-            activation = comb[layer * 2 + 1]
-            wavelet = wavelets[wavelet_index] 
-            layer_out = activation(wavelet @ layer_out) 
-            # note: for layer = n, it is interpreted as the output of layer n+1
+    # # save each layers output and each statistical moment separately
+    # for ind, comb in enumerate(combinations):
+    #     layer_out = x 
+    #     for layer in range(num_layers):
+    #         wavelet_index = comb[layer * 2]
+    #         activation = comb[layer * 2 + 1]
+    #         wavelet = wavelets[wavelet_index] 
+    #         layer_out = activation(wavelet @ layer_out) 
+    #         # note: for layer = n, it is interpreted as the output of layer n+1
 
-            # compute moments on the layer output 
-            for moment in range(1, highest_moment + 1):
-                feat = np.sum(np.power(layer_out, moment))
-                coeff_dict[(layer, moment)].append(feat)
+    #         # compute moments on the layer output 
+    #         for moment in range(1, highest_moment + 1):
+    #             feat = np.sum(np.power(layer_out, moment))
+    #             coeff_dict[(layer, moment)].append(feat)
     
-    # write all of coeff_dict to memory
-    for layer in range(num_layers):
-        for moment in range(1, highest_moment + 1):
-            np_array = np.array(data_list)
-            full_path = os.path.join(save_path, f"layer_{layer}_moment_{moment}.npy")
-            np.save(full_path, np_array)
+    # # write all of coeff_dict to memory
+    # for layer in range(num_layers):
+    #     for moment in range(1, highest_moment + 1):
+    #         np_array = np.array(coeff_dict[(layer, moment)])
+    #         full_path = os.path.join(save_path, f"layer_{layer}_moment_{moment}.npy")
+    #         np.save(full_path, np_array)
+    #         # note: this is not correct! saving at each layer results in duplicates
     
 
      
---
+++ calculate_scattering.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import os
+import blis.models.scattering_transform as st 
+import blis.models.wavelets as wav 
+from blis import DATA_DIR
+import argparse
+import time
+
+# example usage: python calculate_scattering.py --scattering_type blis --wavelet_type W2 --largest_scale 4 --highest_moment 3 --dataset traffic --sub_dataset PEMS08
+
+def validate_args(args):
+    # Check if dataset is 'traffic' and sub_dataset is valid
+    if args.dataset == 'traffic' and args.sub_dataset not in ['PEMS08', 'PEMS07', 'PEMS04', 'PEMS03']:
+        raise ValueError("Invalid sub_dataset for dataset 'traffic'.")
+    # Check if dataset is 'partly_cloudy' and sub_dataset is valid
+    if args.dataset == 'partly_cloudy':
+        if not (0 <= int(args.sub_dataset) <= 155):
+            raise ValueError("Invalid sub_dataset for dataset 'partly_cloudy'. Should be between 0 and 155.")
+    if args.dataset == 'synthetic':
+        raise ValueError("Synthetic still needs to be incorporated")
+
+def main():
+    parser = argparse.ArgumentParser(description="Parse arguments for the program.")
+
+    parser.add_argument("--scattering_type", choices=['blis', 'modulus'], help="Type of scattering: 'blis' or 'modulus'.")
+    parser.add_argument("--wavelet_type", choices=['W1', 'W2'], default = 'W2', help="Type of wavelet: 'W1' or 'W2'.")
+    parser.add_argument("--largest_scale", type=int, help="Largest (dyadic) scale as a positive integer.")
+    parser.add_argument("--highest_moment", type=int, default=1, help="Highest moment as a positive integer. Defaults to 1.")
+    parser.add_argument("--dataset", choices=['traffic', 'partly_cloudy', 'synthetic'], help="Dataset: 'traffic' or 'partly_cloudy' or 'synthetic'.")
+    parser.add_argument("--sub_dataset", help="Sub-dataset value depending on the dataset chosen.")
+    parser.add_argument("--num_layers", type=int, default=3, help="Largest scattering layer")
+
+    args = parser.parse_args()
+
+    # Additional validation for the arguments
+    validate_args(args)
+
+    dataset_dir = os.path.join(DATA_DIR, args.dataset, args.sub_dataset)
+    processed_dir = os.path.join(dataset_dir, 'processed', args.scattering_type, f'largest_scale_{args.largest_scale}')
+    if not os.path.exists(processed_dir):
+        # If not, create the directory
+        os.makedirs(processed_dir)
+
+    A = np.load(os.path.join(dataset_dir, 'adjacency_matrix.npy'))
+
+    import pdb; pdb.set_trace()
+    x = np.load(os.path.join(dataset_dir, 'graph_signals.npy'))
+
+    if args.wavelet_type == 'W2':
+        wavelets = wav.get_W_2(A, args.largest_scale, low_pass_as_wavelet=(args.scattering_type == 'blis'))
+    else:
+        wavelets = wav.get_W_1(A, args.largest_scale, low_pass_as_wavelet=(args.scattering_type == 'blis'))
+    
+    st.scattering_transform(x, args.scattering_type, wavelets, args.num_layers, args.highest_moment, processed_dir)
+    
+    
+
+
+
+
+
+if __name__ == "__main__":
+    start_time = time.time()  # Record the start time
+    main()
+    end_time = time.time()  # Record the end time
+    
+    elapsed_time = end_time - start_time  # Compute the elapsed time
+    print(f"Elapsed time: {elapsed_time:.2f} seconds")
+
+# information needed
+# scattering type
+# wavelet type 
+# largest scale 
+# highest moment 
+# dataset 
+# specific count within the dataset
+
+
+# print('hello world')
+# print(DATA_DIR)
+# traffic_path = os.path.join(DATA_DIR, 'traffic')
+
+# print(os.listdir(traffic_path))
