df633d0ab144f1209d9b4987e7c44dfad9395d18
HungryAmoeba <charles.xu@yale.edu>
2023-10-02 15:21:44 -0400

    data utils

---
+++ blis/data/load_from_np.py
@@ -0,0 +1,31 @@
+import torch 
+from torch_geometric.data import Data
+from torch_geometric.loader import DataLoader
+
+# Convert adjacency matrix to edge indices
+def adjacency_to_edge_indices(A):
+    edge_indices = torch.nonzero(A, as_tuple=False).t()
+    return edge_indices
+
+# Create a dataset and data loader
+def create_dataset(X, y, A):
+    edge_index = adjacency_to_edge_indices(A)
+    data_list = []
+    
+    for i in range(X.shape[0]):
+        num_nodes = X[i].shape[0]
+        data = Data(x=X[i], edge_index=edge_index, y=y[i])
+        data_list.append(data)
+        
+    return data_list
+
+# usage:
+# kf = KFold(n_splits=num_folds, shuffle=True, random_state=42)
+# # Iterate over the folds
+# for train_index, test_index in kf.split(X, y):
+#     X_train, X_test = X[train_index], X[test_index]
+#     y_train, y_test = y[train_index], y[test_index]
+
+#     # Create the dataset and data loader
+#     train_data = create_dataset(X_train, y_train, A)
+#     train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
