97171716bcdcc31aba6c336abd343a3d8ce9ce90
edebrouwer <edward.debrouwer@gmail.com>
2023-10-02 16:59:51 -0400

    classify with GNN torch geometric

--- scripts/classify_torch.py
+++ scripts/classify_torch.py
@@ -1,6 +1,7 @@
 from blis.data import traffic, cloudy, synthetic
 from blis.models.GCN import GCN
 import argparse
+import torch
 
 
 def main(args):
@@ -32,10 +33,16 @@ def main(args):
         if args.model == "GCN":
             model = GCN(in_features = input_dim, hidden_channels = args.hidden_dim, num_classes = num_classes )
         
-
         for epoch in range(args.epochs):
-            ### TRAINING LOOP
-            break 
+
+            for i,b in enumerate(train_dl):
+                model.zero_grad()
+                preds = model(b)
+                loss = 0
+                #COMPLETE LOSS FUNCTION
+            
+
+            
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Parse arguments for the program.")
@@ -43,7 +50,8 @@ if __name__ == "__main__":
     parser.add_argument("--dataset", choices=['traffic', 'partly_cloudy', 'synthetic'], help="Dataset: 'traffic' or 'partly_cloudy' or 'synthetic'.")
     parser.add_argument("--sub_dataset", help="Sub-dataset value depending on the dataset chosen.")
     parser.add_argument("--task_type", type=str,  help="The task type to use for the classification")
-    parser.add_argument("--model", type=str, default="LR", help="Classification model to use")
+    parser.add_argument("--model", type=str, default="GCN", help="Classification model to use")
+    parser.add_argument("--hidden_dim", type=int, default=32, help="Number of hidden channels in the GNN model")
     parser.add_argument("--epochs", type=int, default=100, help="Number of epochs to train for")
 
     args = parser.parse_args()
